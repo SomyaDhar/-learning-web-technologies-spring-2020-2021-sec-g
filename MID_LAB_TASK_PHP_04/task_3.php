0346cd7c7369e7d6a6492839ecfeb4e381a7bc29
Somya Dhar <shomyodhar100@gmail.com>
2021-02-17 21:58:40 +0600

    task3

---
+++ MID_LAB_TASK_PHP_04/task_3.php
@@ -0,0 +1,15 @@
+<?php
+
+    $num = 20;
+    if($num % 2 == 0)
+    {
+        echo $num." is an even number";
+        
+    }
+
+    else
+    {
+        echo $num." is an odd number";
+    }
+
+?>
