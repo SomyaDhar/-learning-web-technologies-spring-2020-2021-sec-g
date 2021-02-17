109d71ccde3c4cbbac5f388b7de62828eb697e33
Somya Dhar <shomyodhar100@gmail.com>
2021-02-17 22:05:46 +0600

    tas5

---
+++ MID_LAB_TASK_PHP_04/task_5.php
@@ -0,0 +1,17 @@
+<?php
+
+    for($num = 10; $num <=100; $num++)
+    {
+        if($num % 2 == 0)
+        {
+            continue;
+        }
+    
+        else
+        {
+            echo $num .;
+        }
+    }
+
+
+?>
