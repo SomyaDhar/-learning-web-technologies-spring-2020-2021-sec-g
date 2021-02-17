87856051e74d6545a93abea269ac43f6bd81665d
Somya Dhar <shomyodhar100@gmail.com>
2021-02-17 22:13:13 +0600

    task 6

---
+++ MID_LAB_TASK_PHP_04/task_6.php
@@ -0,0 +1,19 @@
+<?php
+
+    
+
+    $arr = array(1, 2, 3, 5, 6, 7, 9);
+    
+    $search = 4;
+
+    for($i=0; $i<7; $i++)
+    {
+        if($arr[$i] == $search)
+        {
+            echo "Number found";
+        }
+
+    }
+
+
+?>
