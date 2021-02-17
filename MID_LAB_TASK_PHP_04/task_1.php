d0c354119d5cce50d45e4d0ffc023b3a7bf40e88
Somya Dhar <shomyodhar100@gmail.com>
2021-02-17 21:53:45 +0600

    task 1

---
+++ MID_LAB_TASK_PHP_04/task_1.php
@@ -0,0 +1,12 @@
+<?php
+
+    $length = 2;
+    $width = 3;
+
+    $area = $length * $width;
+    $perimeter = 2 * ($length * $width);
+
+    echo "Area is: ".$area;
+    echo "<br>";
+    echo "Permiter is : ".$perimeter;
+?>
