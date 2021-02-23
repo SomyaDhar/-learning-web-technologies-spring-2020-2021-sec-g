5ed82e2d5b1b11cac71fe71edfe5f69e9dd48288
Somya Dhar <shomyodhar100@gmail.com>
2021-02-23 09:57:50 +0600

    task 3

---
+++ MID_LAB_TASK__PHP_06/task_3_profile/task_3_profile _pic.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>File Input field</title>
+</head>
+    <body>
+    <form method="POST" action="#">
+        <fieldset>
+            <legend>Profile Picture</legend>
+            <br><img src="C.jpg" alt=""><br>
+            User ID <input type="text" width="10px"><br>
+
+            <br/>Picture <input type="file">
+        <br>
+    </fieldset>
+    <br><input type="button" value="Submit"/>
+    </form>
+    </body>
+</html>
