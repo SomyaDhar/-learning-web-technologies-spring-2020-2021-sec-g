df858d014d99f311817b5fdeeb977b0d8c118377
Somya Dhar <shomyodhar100@gmail.com>
2021-03-02 00:18:38 +0600

    Task D

---
+++ MID_LAB_TASK_PHP_SESSION_07/FORGOT_PASSWORD.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Forgot Password</title>
+  
+        <table align='center' border="1" width="1100px">
+                         <tr>           
+                            <th> <img src="logo.png" width=160px height=100px align="left">
+                            <p align='right'>  <a href="Public_Home.html" align='right';>Home</a> |
+                                <a href="LOGIN.php" align='right';>Login</a> |
+                                <a href="REGISTRATION.html" aslign='right';>Registration</a> </p>
+        
+                            </th>
+                            
+                        </tr> 
+                    
+                        <tr>
+                        <td height=240px;>
+                            
+							<form method="POST" action="Forgot_Password_Check.php">
+								<fieldset>
+										<legend><b>FORGOT PASSWORD</b> </legend>
+							
+											Enter Email  : <input type ="text" name="email"> </input><br><hr>
+										
+											<br>
+													<input type="submit" name="submit" value="Submit">
+									</fieldset>
+								</form>
+                        </td>
+                    </tr>
+                   
+        
+                    <td colspan="3">
+                        <h6 style="text-align: center;">Copyright  @2017</h6>
+                    </td>
+        </table>
+    
+    </form>
+</body>
+</html>
